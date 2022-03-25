749e7e4fc7f16e0dbd27c240bab0290e2072d706
5t3ph <thinkdobecreate@gmail.com>
2022-03-25 10:00:07 -0500

    Part 4: Move docs, begin layout utils

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
 
+  if (process.env.ELEVENTY_ENV === "prod") {
+    eleventyConfig.ignores.add("./src/__docs/");
+  }
+
   return {
     dir: {
       input: "src",
